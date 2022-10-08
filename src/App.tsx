2b14042dc51d52c8a5c230fb752a149d7453a466
myuon <ioi.joi.koi.loi@gmail.com>
2022-10-08 14:35:48 +0900

    chore: emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { css } from "@emotion/react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -27,6 +28,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div
+        css={css`
+          background-color: #f9f9f9;
+          border: 1px solid #ddd;
+          padding: 10px;
+        `}
+      >
+        EMOTION INSTALLED
+      </div>
     </div>
   );
 }
